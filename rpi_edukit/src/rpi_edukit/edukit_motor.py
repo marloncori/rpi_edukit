28c9f90bc23106e7a952926e6f2a8d46b51bd414
marloncori <84450525+marloncori@users.noreply.github.com>
2021-08-27 10:27:23 +0200

    Update edukit_motor.py

--- rpi_edukit/src/rpi_edukit/edukit_motor.py
+++ rpi_edukit/src/rpi_edukit/edukit_motor.py
@@ -24,32 +24,28 @@ class Motor(Arduino):
         self.pinE.write(state)
         
     def clockwise(self, speed):
-        self._pwm = speed + 1
-        for spin in range(self._minSpeed, self._pwm, self._offset): 
+        for spin in range(self._minSpeed, speed, self._offset): 
             self.__setMotor(spin, 0, 1)
-            sleep(0.04)
+            sleep(0.05)
             
     def counterclockwise(self, speed):
-        self._pwm = speed + 1
-        for spin in range(self._minSpeed, self._pwm, self._offset):
+        for spin in range(self._minSpeed, speed, self._offset):
             self.__setMotor(0, spin, 1)
-            sleep(0.04)
+            sleep(0.05)
         
     def stop(self):
         self._pwm = 0
-        self.__setMotor(self._pwm, self._pwm, 0)
+        self.__setMotor(0, 0, 0)
 
     def close(self):
         self.__setMotor(0,0,0)
         self.uno.exit()
     
     def _speed_map(self, val, i_min, i_max, o_min, o_max):
-        self._pwm = round(o_min + (o_max - o_min) * ((val - i_min) / (i_max - i_min)))
-        return self._pwm
+        return round(o_min + (o_max - o_min) * ((val - i_min) / (i_max - i_min)))
     
     def _speed_map_abs(self, val, i_min, i_max, o_min, o_max):
-        self._pwm = abs(round(o_min + (o_max - o_min) * ((val - i_min) / (i_max - i_min))))
-        return self._pwm
+        return abs(round(o_min + (o_max - o_min) * ((val - i_min) / (i_max - i_min))))
     
     def _clip(self, value, minim, maxim):
         #Ensure value is between minimum and maximum.
@@ -62,9 +58,9 @@ class Motor(Arduino):
     def move(self, speedPercent):
         speed = self._clip(abs(speedPercent), self._minSpeed, self._maxSpeed)
         #positive speed moves wheels forwards, negative - backwards
-        if speedPercent < 0:
-            self._speed_map_abs(speed, self._minSpeed, self._maxSpeed, 0, 256)
-            self.counterclockwise(speed)
+        if speedPercent < 0.0:
+            self._pwm = self._speed_map_abs(speed, -1.0, 0.0, self._minSpeed, self._maxSpeed)
+            self.counterclockwise(self._pwm)
         else:
-            self._speed_map(speed, self._minSpeed, self._maxSpeed, 0, 256)
-            self.clockwise(speed)
+            self._pwm = self._speed_map(speed, 0.0, 1.0, self._minSpeed, self._maxSpeed)
+            self.clockwise(self._pwm)
